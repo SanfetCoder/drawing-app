200d267719566ee74600dbe3abbafddc95ec42c9
sanphetsongjindasak <sanphetsongjindasak@gmail.com>
2026-02-05 21:03:52 +0700

    feat : change color of the stroke

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ const MAXIMUM_LINE_WIDTH: number = 100;
 function App() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const [lineWidth, setLineWidth] = useState<number>(MINIMUM_LINE_WIDTH);
+  const [strokeColor, setStrokeColor] = useState<string>('#000000');
 
   useEffect(() => {
     function drawRectangle() {
@@ -19,7 +20,7 @@ function App() {
       ctx.lineTo(400.5, 0);
       ctx.lineTo(400, 400);
       ctx.lineTo(0, 400);
-      ctx.strokeStyle = 'blue';
+      ctx.strokeStyle = strokeColor;
       ctx.lineWidth = lineWidth;
       ctx.closePath();
 
@@ -27,16 +28,26 @@ function App() {
     }
 
     drawRectangle();
-  }, [lineWidth]);
+  }, [lineWidth, strokeColor]);
   return (
     <main>
-      <canvas ref={canvasRef} key={lineWidth} width={400} height={400}></canvas>
+      <canvas
+        ref={canvasRef}
+        key={`${lineWidth}-${strokeColor}`}
+        width={400}
+        height={400}
+      ></canvas>
       <input
         type="range"
         min={MINIMUM_LINE_WIDTH}
         max={MAXIMUM_LINE_WIDTH}
         value={lineWidth}
         onChange={(e) => setLineWidth(Number(e.target.value))}
+      />
+      <input
+        type="color"
+        value={strokeColor}
+        onChange={(e) => setStrokeColor(e.target.value)}
       />
       <h3>Line Width: {lineWidth}</h3>
     </main>
