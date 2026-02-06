8aa222cca3639688197eb3b43018e5634e329e6f
sanphetsongjindasak <sanphetsongjindasak@gmail.com>
2026-02-06 13:41:17 +0700

    refactor(draw) : useDraw hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import './App.css';
-import { useMousePosition } from './shared/hooks/useMousePosition';
-import { useDetectMouseClicked } from './shared/hooks/useDetectMouseClicked';
+import { useDraw } from './features/drawing/hooks/useDraw';
 
 const MINIMUM_LINE_WIDTH: number = 10;
 const MAXIMUM_LINE_WIDTH: number = 100;
@@ -10,25 +9,8 @@ function App() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const [lineWidth, setLineWidth] = useState<number>(MINIMUM_LINE_WIDTH);
   const [strokeColor, setStrokeColor] = useState<string>('#000000');
-  const isMouseClicked = useDetectMouseClicked();
-  const mousePosition = useMousePosition();
 
-  useEffect(() => {
-    if (!mousePosition || !isMouseClicked) return;
-
-    const canvas = canvasRef.current;
-    if (!canvas) return;
-
-    const ctx = canvas.getContext('2d');
-    if (!ctx) return;
-
-    const { left: canvasLeft, top: canvasTop } = canvas.getBoundingClientRect();
-
-    const [mouseX, mouseY] = [mousePosition.x, mousePosition.y];
-
-    ctx.lineTo(mouseX - canvasLeft, mouseY - canvasTop);
-    ctx.stroke();
-  }, [mousePosition?.x, mousePosition?.y, isMouseClicked]);
+  useDraw(canvasRef);
 
   useEffect(() => {
     const ctx = canvasRef.current?.getContext('2d');
@@ -54,7 +36,6 @@ function App() {
         onChange={(e) => setStrokeColor(e.target.value)}
       />
       <h3>Line Width: {lineWidth}</h3>
-      <h3>{isMouseClicked ? 'clicked' : 'released'}</h3>
     </main>
   );
 }
---
+++ src/features/drawing/hooks/useDraw.ts
@@ -0,0 +1,25 @@
+import { useEffect, type RefObject } from 'react';
+import { useMousePosition } from '../../../shared/hooks/useMousePosition';
+import { useDetectMouseClicked } from '../../../shared/hooks/useDetectMouseClicked';
+
+export function useDraw(canvasRef: RefObject<HTMLCanvasElement | null>) {
+  const mousePosition = useMousePosition();
+  const isMouseClicked = useDetectMouseClicked();
+
+  useEffect(() => {
+    if (!mousePosition || !isMouseClicked) return;
+
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
+
+    const { left: canvasLeft, top: canvasTop } = canvas.getBoundingClientRect();
+
+    const [mouseX, mouseY] = [mousePosition.x, mousePosition.y];
+
+    ctx.lineTo(mouseX - canvasLeft, mouseY - canvasTop);
+    ctx.stroke();
+  }, [mousePosition?.x, mousePosition?.y, isMouseClicked]);
+}
