82fd3cc67828d3fe5727c339ae3b623b9d35b9f6
sanphetsongjindasak <sanphetsongjindasak@gmail.com>
2026-02-06 14:23:20 +0700

    feat(drawing) : draw when the mouse is clicked

--- src/features/drawing/hooks/useDraw.ts
+++ src/features/drawing/hooks/useDraw.ts
@@ -1,5 +1,8 @@
 import { useEffect, type RefObject } from 'react';
-import { useMousePosition } from '../../../shared/hooks/useMousePosition';
+import {
+  useMousePosition,
+  type MousePosition,
+} from '../../../shared/hooks/useMousePosition';
 import { useDetectMouseClicked } from '../../../shared/hooks/useDetectMouseClicked';
 
 export function useDraw(
@@ -7,20 +10,27 @@ export function useDraw(
   { lineWidth, strokeColor }: { lineWidth: number; strokeColor: string },
 ) {
   const isMouseClicked = useDetectMouseClicked();
+  const mousePosition = useMousePosition();
 
-  useDrawLineWhenMouseMoving(canvasRef, isMouseClicked);
+  useDrawLineWhenMouseMoving(canvasRef, isMouseClicked, mousePosition);
 
   useBeginNewPathWhenMouseLifted(canvasRef, isMouseClicked);
 
   useSetCanvasStyle(canvasRef, lineWidth, strokeColor);
+
+  useDrawLineWhenMousePressed(
+    canvasRef,
+    lineWidth,
+    isMouseClicked,
+    mousePosition,
+  );
 }
 
 function useDrawLineWhenMouseMoving(
   canvasRef: RefObject<HTMLCanvasElement | null>,
   isMouseClicked: boolean,
+  mousePosition: MousePosition | null,
 ) {
-  const mousePosition = useMousePosition();
-
   useEffect(() => {
     if (!mousePosition || !isMouseClicked) return;
 
@@ -70,4 +80,31 @@ function useSetCanvasStyle(
     ctx.lineWidth = lineWidth;
     ctx.strokeStyle = strokeColor;
   }, [lineWidth, strokeColor]);
+}
+
+function useDrawLineWhenMousePressed(
+  canvasRef: RefObject<HTMLCanvasElement | null>,
+  lineWidth: number,
+  isMouseClicked: boolean,
+  mousePosition: MousePosition | null,
+) {
+  useEffect(() => {
+    if (!mousePosition || !isMouseClicked) return;
+
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
+
+    const { left: canvasLeft, top: canvasTop } = canvas.getBoundingClientRect();
+
+    ctx.rect(
+      mousePosition.x - canvasLeft,
+      mousePosition.y - canvasTop,
+      lineWidth,
+      lineWidth,
+    );
+    ctx.fill();
+  }, [mousePosition?.x, mousePosition?.y, isMouseClicked, lineWidth]);
 }
